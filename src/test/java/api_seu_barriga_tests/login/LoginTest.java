625d07ccc8c1c7830fbaa8236e537f6e5ff40636
Andre_dev_ALS <andreluizsilvasilva2019@gmail.com>
2023-08-21 22:03:27 -0300

    - conclusão de cenário - 1. completo o cenário de id_0002DevoAcessarApiComSucesso

--- src/test/java/api_seu_barriga_tests/login/LoginTest.java
+++ src/test/java/api_seu_barriga_tests/login/LoginTest.java
@@ -4,13 +4,20 @@ import static io.restassured.RestAssured.given;
 
 import org.junit.Test;
 
+import api_seu_barriga_tests.model.LoginModel;
 import me.wcaquino.seubarriga.core.BaseTest;
 
 public class LoginTest extends BaseTest {
+	private LoginModel dadosAcesso;
 
 	@Test
-	public void naoDeveAcessarApiSemToken() {
+	public void id_0001NaoDeveAcessarApiSemToken() {
 		given().when().get("/contas").then().statusCode(401);
 	}
 
+	@Test
+	public void id_0002DevoAcessarApiComSucesso() {
+		dadosAcesso = new LoginModel("emailparatestesautomacao@gmail.com", "automacaoteste");
+		given().body(dadosAcesso).when().post("/signin").then().statusCode(200).log().all();
+	}
 }
---
+++ src/test/java/api_seu_barriga_tests/model/LoginModel.java
@@ -0,0 +1,30 @@
+package api_seu_barriga_tests.model;
+
+public class LoginModel {
+	private String email;
+	private String senha;
+
+	public LoginModel(String email, String senha) {
+		setEmail(email);
+		setSenha(senha);
+		;
+
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	private void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	private void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
